57bc7e670b2bcf8619cf4cf458dd08f637d68278
Zhuoning Yuan <38697082+yzhuoning@users.noreply.github.com>
2022-10-15 23:57:04 -0400

    Update train.py

--- train.py
+++ train.py
@@ -379,11 +379,5 @@ def adjust_learning_rate(optimizer, epoch, args):
     return lr
 
 
-def adjust_moco_momentum(epoch, args):
-    """Adjust moco momentum based on current epoch"""
-    m = 1. - 0.5 * (1. + math.cos(math.pi * epoch / args.epochs)) * (1. - args.moco_m)
-    return m
-
-
 if __name__ == '__main__':
     main()
